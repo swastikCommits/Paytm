c9a2a1336cd009f39b9a21f6a5d534ef9f6c6792
onyx <swastikrpanigrahi@gmail.com>
2024-11-08 15:35:48 +0530

    signin route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -52,9 +52,41 @@ router.post("/signup", async (re, res)=>{
     })
        
    
+})
 
-    
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
 
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
 })
 
 
