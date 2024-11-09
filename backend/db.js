636acf95bbb0aa5f2055010b5bcfbcb83f78286a
swastikCommits <textswastik.alt@gmail.com>
2024-11-09 10:00:08 +0530

    bank schema added

--- backend/db.js
+++ backend/db.js
@@ -2,36 +2,29 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://swastikrpanigrahi:nvmPxMJh6kHKpf9L@0nyx.mdjy9.mongodb.net/paytm');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        minLength: 3,
-        maxLength: 30
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6
-    },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    }
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String
 });
 
-const User=mongoose.model('User', userSchema);
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
 
-module.exports={
-    User
-}
+
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = {
+	User, 
+    Account,
+};
