7771e15fe027112a1acaf6c6b83eab45e42d553d
swastikCommits <textswastik.alt@gmail.com>
2024-11-17 12:53:26 +0530

    Frontend connected with backend

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,3 +1,4 @@
+
 export const Appbar = () => {
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,3 +1,4 @@
+
 export const Balance = ({ value }) => {
     return <div className="flex">
         <div className="font-bold text-lg">
